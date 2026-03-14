69f23e116a17a8455ffe5c9bdf5346fd7745cb80
meghananvekar <megha.bhat3@gmail.com>
2026-03-14 10:53:03 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,8 @@ public class hello {
     public static void main(String[] args) {
         // Print the message to the console
         System.out.println("Hello");
+         System.out.println("megha");
+        
     }
 }
 
